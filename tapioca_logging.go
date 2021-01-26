1c9e0b544b8a72326b205f4e4bf7099e12d78fb2
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2021-01-26 14:47:35 -0600

    first commit

---
+++ tapioca_logging.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func Logging(message string){
+
+	f, err := os.OpenFile("testlogfile", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println(message)
+	fmt.Println(message)
+}
+
+func main(){
+	Logging("An error has occured")
+}
