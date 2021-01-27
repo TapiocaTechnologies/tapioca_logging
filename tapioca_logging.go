5c65f10d9c849c030176c5de962594bd25327342
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2021-01-26 18:07:36 -0600

    added LoggingError

--- tapioca_logging.go
+++ tapioca_logging.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	
 )
+var file_name = ""
+func SetLogFile(file string){
+	file_name = file
+}
 func Logging(message string){
-
-	f, err := os.OpenFile("testlogfile", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	
+	f, err := os.OpenFile(file_name, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
@@ -16,5 +21,17 @@ func Logging(message string){
 	log.SetOutput(f)
 	log.Println(message)
 	fmt.Println(message)
+}
+
+func LoggingError(e error) {
+	f, err := os.OpenFile(file_name, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println(e)
+	fmt.Println(e)
 }
 
